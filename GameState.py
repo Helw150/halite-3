3aab5126eafe05c2a9c2e235e5de0f028e47d1fc
Helw150 <wbh230@nyu.edu>
2018-12-11 22:14:58 +0400

    Prepare comments for possible blog when the competition ends

--- GameState.py
+++ GameState.py
@@ -1,3 +1,8 @@
+'''
+This file is the high level bot strategy for the full fleet 
+of ships (or turtles!) in my Halite 3 Bot. It also houses the logic
+for transferring state from the hlt package provided by TwoSigma to formats that I find more usable.
+'''
 import hlt
 from hlt import constants
 from hlt.positionals import Direction, Position
@@ -5,6 +10,8 @@ from ShipActions import harvest
 import numpy as np
 
 import logging
+
+# Map from width of the map to the number of turns in the game
 widthToTurns = {32:400, 40:425, 48:450, 56:475, 64:500}
 
 class GameState():
@@ -16,58 +23,7 @@ class GameState():
         self.game.ready("Helw150")
         logging.info("Beginning Game! My Player ID is {}.".format(self.game.my_id))
 
-    def createPositionWeights(self):
-        origin = Position(0,0)
-        origin_distance_matrix = np.zeros((self.width, self.width), dtype=float)
-        for x in range(self.width):
-            for y in range(self.width):
-                next_position = Position(x, y)
-                distance = float(self.game_map.calculate_distance(origin, next_position))
-                offset = 1
-                if distance == 0:
-                    offset = 0
-                origin_distance_matrix[x][y] = float(self.game_map.calculate_distance(origin, next_position)) + offset
-        origin_weight_matrix = (0.4)**origin_distance_matrix
-        self.position_weights = {origin: origin_weight_matrix}
-        for x in range(self.width):
-            for y in range(self.width):
-                position = Position(x, y)
-                weight_matrix_horizontal_shifted = np.roll(origin_weight_matrix, x, axis=0)
-                weight_matrix_shifted = np.roll(weight_matrix_horizontal_shifted, y, axis=1)
-                self.position_weights[position] = weight_matrix_shifted
-                
-
-    def updateHaliteMatrix(self):
-        self.halite_matrix = self.game_map.halite_matrix
-        logging.info(self.halite_matrix)
-        
-    def spawningParams(self):
-        self.end_repro = 200
-        self.width = self.game.game_map.width
-        self.turns = widthToTurns[self.width]
-        self.max_ships = self.turns*.075
-        
-    def shipsByMovingStatus(self):
-        ships = self.me.get_ships()
-        still_ships = [ship for ship in ships if ship.halite_amount < self.game_map[ship.position].halite_amount*0.1 and self.game_map[ship.position].halite_amount != 0]
-        ships_to_move = [ship for ship in ships if ship not in still_ships]
-        ships_to_move.sort(key = lambda x: self.game_map.calculate_distance(x.position, self.me.shipyard.position))
-        return still_ships, ships_to_move
-        
-    def get_halite_grid(self):
-        self.halite_values = []
-        for row in self.game_map._cells:
-            row_halite = []
-            for cell in row:
-                row_halite.append(cell.halite_amount)
-            self.halite_values.append(row_halite)
-        
-    def updateStates(self):
-        self.me = self.game.me
-        self.game_map = self.game.game_map
-        self.updateHaliteMatrix()
-        self.futures = {}
-            
+    # High level operations that are run each turn
     def loop(self):
         commands = []
         self.game.update_frame()
@@ -76,8 +32,75 @@ class GameState():
         commands.extend(self.spawn())
         logging.info(commands)
         self.game.end_turn(commands)
+    
+    # Populates the weight matrix based on distance from the center of the map
+    def createPositionWeights(self):
+        origin = Position(0,0)
+        origin_distance_matrix = np.zeros((self.width, self.width), dtype=float)
+        # Creates basic distance matrix
+        for x in range(self.width):
+            for y in range(self.width):
+                next_position = Position(x, y)
+                distance = float(self.game_map.calculate_distance(origin, next_position))
+                offset = 1
+                if distance == 0:
+                    offset = 0
+                origin_distance_matrix[x][y] = float(self.game_map.calculate_distance(origin, next_position)) + offset
+        # Exponential function to balance local and global focus
+        # FUTURE: Tune this with TPOT package rather than by hand
+        origin_weight_matrix = (0.4)**origin_distance_matrix
+        self.position_weights = {origin: origin_weight_matrix}
+        # Use roll to quickly create weight matrix for every position in the map
+        for x in range(self.width):
+            for y in range(self.width):
+                position = Position(x, y)
+                weight_matrix_horizontal_shifted = np.roll(origin_weight_matrix, x, axis=0)
+                weight_matrix_shifted = np.roll(weight_matrix_horizontal_shifted, y, axis=1)
+                self.position_weights[position] = weight_matrix_shifted
+                
 
+    # Pass data from internal to custom state manager
+    def updateHaliteMatrix(self):
+        self.halite_matrix = self.game_map.halite_matrix
+        logging.info(self.halite_matrix)
 
+    # State setup for spawning of new shapes
+    # FUTURE: Tune with TPOT rather than by hand
+    def spawningParams(self):
+        self.end_repro = 200
+        self.width = self.game.game_map.width
+        self.turns = widthToTurns[self.width]
+        self.max_ships = self.turns*.075
+
+    # Split ships by whether or not they can afford the cost to move
+    def shipsByMovingStatus(self):
+        ships = self.me.get_ships()
+        # Ships that have no choice but to stay still
+        still_ships = [ship for ship in ships if ship.halite_amount < self.game_map[ship.position].halite_amount*0.1 and self.game_map[ship.position].halite_amount != 0]
+        ships_to_move = [ship for ship in ships if ship not in still_ships]
+        # FUTURE: This is meant to help the collision avoidance algorithm avoid collisions with fewer recursions.
+        # It does so somewhat, but really I want to cluster the ships so that collision avoidance schedules them in
+        # batches somehow. Doubt it would actually be a huge performance increase so saving for later.
+        ships_to_move.sort(key = lambda x: self.game_map.calculate_distance(x.position, self.me.shipyard.position))
+        return still_ships, ships_to_move
+
+    # State transfer and reformating from Halite internals
+    def get_halite_grid(self):
+        self.halite_values = []
+        for row in self.game_map._cells:
+            row_halite = []
+            for cell in row:
+                row_halite.append(cell.halite_amount)
+            self.halite_values.append(row_halite)
+
+    # Turn by turn state update
+    def updateStates(self):
+        self.me = self.game.me
+        self.game_map = self.game.game_map
+        self.updateHaliteMatrix()
+        self.futures = {}
+
+    # Get the position the ship is planning to move to in the next turn and store it in state
     def addToFuture(self, ship, direction):
         logging.info(self.futures)
         future_pos = ship.position.directional_offset(direction)
@@ -87,25 +110,32 @@ class GameState():
         else:
             self.futures[future_pos] = [(ship, direction)]
         logging.info(self.futures)
-        
+
+    # Move ships towards targets while avoiding collisions with friendly ships
     def moveShips(self):
         still_ships, ships_to_move = self.shipsByMovingStatus()
         for ship in still_ships:
             self.addToFuture(ship, Direction.Still)
         for ship in ships_to_move:
+            # This if condition overrides collision avoidance for the end game
+            # so that ships will kamikaze all of the collected resources into the base
+            # before the final turn.
             if ship.position != self.me.shipyard.position or self.turns_left > 25:
                 move = harvest(self, ship)
                 self.addToFuture(ship, move)
+        # Keep re-routing colliding friendly ships until no friendly ships collide
         while self.collisionDetected():
             self.collisionResolve()
         return self.enactFuture()
 
+    # Detect if two friendly ships have the same future position
     def collisionDetected(self):
         for residing_ships in self.futures.values():
             if len(residing_ships) > 1:
                 return True
         return False
 
+    # Re-route ships that are currently on course for collision
     def collisionResolve(self):
         ships_to_resolve = []
         for position in self.futures:
@@ -117,6 +147,7 @@ class GameState():
             move = harvest(self, ship)
             self.addToFuture(ship, move)
 
+    # Convert the future positions into commands for the game engine
     def enactFuture(self):
         moves = []
         for position in self.futures:
@@ -124,10 +155,12 @@ class GameState():
             moves.append(ship.move(move))
         return moves
 
+    # Helper for the number of turns still in the game
     @property
     def turns_left(self):
         return self.turns-self.game.turn_number
-    
+
+    # Rules which define whether or not to spawn new ships
     def spawn(self):
         spawns = []
         if self.turns_left >= self.end_repro and self.me.halite_amount >= constants.SHIP_COST and self.me.shipyard.position not in self.futures and len(self.me.get_ships()) < self.max_ships:
--- ShipActions.py
+++ ShipActions.py
@@ -1,3 +1,6 @@
+'''
+This file controls all ship by ship behaviour for this Halite 3 bot, except for smarter_navigate which was the only modification I made to the vanilla hlt package.
+'''
 import hlt
 from hlt import constants
 from hlt.positionals import Direction
@@ -6,30 +9,43 @@ import numpy as np
 import random
 import logging
 
+# Boolean check for whether a ship should return to base
 def returnCondition(game_state, ship):
+    # Conditions for normal harvesting behaviour
     min_hal = ship.halite_amount >= 0.9*constants.MAX_HALITE
     hal_ratio = game_state.game_map[ship.position].halite_amount <= 0.18 * ship.halite_amount
+    # Two conditions for end-game kamikaze ship strategy
     return_for_end = int(game_state.game_map.calculate_distance(ship.position, game_state.me.shipyard.position)*1.5) > game_state.turns_left
     emergency_return = game_state.turns_left <= 25
     return (min_hal and hal_ratio) or return_for_end or emergency_return or ship.is_full
 
+# High level access point from ShipActions to the rest of the bot
 def harvest(game_state, ship):
     if returnCondition(game_state, ship):
+        # Bring resources back to base
         return(returnToHome(game_state, ship))
     elif ship.position == game_state.me.shipyard.position:
+        # Move ships randomly from the spawn to add a
+        # stochastic exploration element
         next_spot = random.choice(ship.position.get_surrounding_cardinals())
         move = game_state.game_map.smarter_navigate(ship, next_spot, game_state.futures)
         return(move)
     else:
+        # Move to the surrouding position with the
+        # best value as determined by chooseBestCell
         candidates = manhattanRadius(1, ship.position)
         next_spot = chooseBestCell(candidates, game_state, ship.position)
+        # Smarter navigate avoids placing two ships in the same
+        # position by using the values of game_state.futures
         move = game_state.game_map.smarter_navigate(ship, next_spot, game_state.futures)
         return(move)
-        
+
+# Sends ship back to the spawn
 def returnToHome(game_state, ship):
     move = game_state.game_map.smarter_navigate(ship, game_state.me.shipyard.position,game_state.futures)
     return(move)
 
+# Helper function to get positions in a radius, but with only manhattan moves available
 def manhattanRadius(N, position):
     if N == 1:
         return position.get_surrounding_cardinals()
@@ -39,19 +55,26 @@ def manhattanRadius(N, position):
             candidates.extend(manhattanRadius(N-1, next_position))
         return candidates
 
+# Game state evaluator which multiplies the halite amounts on the map by the distance-weight matrix to quickly evaluate how valuable a particular position on the map is
 def chooseBestCell(positions, game_state, current_pos):
     hallite_amount = game_state.game_map[current_pos].halite_amount
+    # Cost to move as defined by the game engine - not tunable
     cost_to_move = hallite_amount * 0.1
+    # How much the ship would harvest defined by the game engine -  not tunable
     gained_by_stay = hallite_amount * 0.25
     position_weight_dict = game_state.position_weights
     halite_matrix = game_state.halite_matrix
+    # The opportunity cost for moving to any other cell from the current position
     max_val = (np.sum(np.multiply(position_weight_dict[current_pos], halite_matrix))+gained_by_stay+cost_to_move)
     max_cell = current_pos
     for position in positions:
         position = game_state.game_map.normalize(position)
+        # Give occupied positions a 0 value
         position_free = int(not position in game_state.futures)
+        # FUTURE: 0.75 is a hand tuned weight and should be done with TPOT instead
         value = np.sum(np.multiply(position_weight_dict[position], halite_matrix))*0.75*position_free
         if value > max_val and position != current_pos:
             max_cell = position
             max_val = value
+    # Return the highest value move the ship has at the moment
     return max_cell
